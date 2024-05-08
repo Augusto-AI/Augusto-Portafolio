9220a341fee052e445340be6baa1711edad5e5d2
Augusto-AI <developer@nestech.us>
2024-05-08 15:35:52 -0500

    4 Update

--- src/components/sections/hero.js
+++ src/components/sections/hero.js
@@ -61,7 +61,7 @@ const Hero = () => {
 
   const one = <h1>Hi, my name is</h1>;
   const two = <h2 className="big-heading">Augusto Herrera.</h2>;
-  const three = <h3 className="big-heading">I Design and build things for the web.</h3>;
+  const three = <h3 className="big-heading">I Design And Build Things For The Web.</h3>;
   const four = (
     <>
       <p>
